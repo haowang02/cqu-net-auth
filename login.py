e8fd92b97062bd4330fd1bf6a1fe4a92cca8eeb2
Hao Wang <haowang02@outlook.com>
2025-03-30 09:28:04 +0800

    优化错误处理

--- login.py
+++ login.py
@@ -2,6 +2,7 @@ import os
 import re
 import sys
 import time
+import json
 import signal
 import socket
 import logging
@@ -58,7 +59,13 @@ def get_account():
 
 
 def login(account: str, password: str, term_type: str, ip: str):
-    """认证校园网"""
+    """认证校园网
+    dr1004({"result":1,"msg":"Portal协议认证成功！"});
+    dr1004({"result":0,"msg":"账号不存在","ret_code":1});
+    dr1004({"result":0,"msg":"密码错误","ret_code":1});
+    dr1004({"result":0,"msg":"认证操作非本机终端！","ret_code":"1"});
+    dr1004({"result":0,"msg":"认证出现异常！","ret_code":"1"});
+    """
     if term_type == 'android':
         url = f"http://10.254.7.4:801/eportal/portal/login?callback=dr1005&login_method=1&user_account=%2C1%2C{account}&user_password={password}&wlan_user_ip={ip}&wlan_user_ipv6=&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=&ua=Mozilla%2F5.0%20(Linux%3B%20Android%208.0.0%3B%20SM-G955U%20Build%2FR16NW)%20AppleWebKit%2F537.36%20(KHTML%2C%20like%20Gecko)%20Chrome%2F134.0.0.0%20Mobile%20Safari%2F537.36%20Edg%2F134.0.0.0&term_type=2&jsVersion=4.2&terminal_type=2&lang=zh-cn&v=9451&lang=zh"
     else:
@@ -67,12 +74,10 @@ def login(account: str, password: str, term_type: str, ip: str):
     try:
         with urllib.request.urlopen(req, timeout=5) as response:
             content = response.read().decode('utf-8')
-            if "Portal协议认证成功！" in content:
-                return True, content
-            else:
-                return False, content
+            result = json.loads(content.strip().strip("dr1004();"))
+            return result["result"], result["msg"]
     except urllib.error.URLError:
-        return False, None
+        return 0, "网络错误"
 
 
 def set_logger(log_level: str):
@@ -90,69 +95,65 @@ def set_logger(log_level: str):
     logger.addHandler(ch)
 
 
-def signal_handler(signum, frame):
-    """
-    信号处理函数，处理 SIGTERM/SIGINT 信号
-    """
-    global logger
-    logger.info(f"Received signal {signum}, exiting...")
-    sys.exit(0)
-
-
 def parse_args():
+    global logger
     parser = argparse.ArgumentParser()
     parser.add_argument("--account", type=str, default=os.getenv("ACCOUNT", ""), help="校园网账户(学/工号)")
     parser.add_argument("--password", type=str, default=os.getenv("PASSWORD", ""), help="校园网密码")
     parser.add_argument("--term_type", type=str, default=os.getenv("TERM_TYPE", "pc"), choices=["android", "pc"], help="登录设备类型")
     parser.add_argument("--log_level", type=str, default=os.getenv("LOG_LEVEL", "info"), choices=["debug", "info"], help="日志级别")
+    parser.add_argument("--interval", type=int, default=os.getenv("INTERVAL", 5), help="检查网络状态的间隔时间(秒)")
     args = parser.parse_args()
-    return args.account, args.password, args.term_type, args.log_level
+    
+    # 验证参数
+    if not args.account or not args.password:
+        logger.error("未指定校园网账户或密码")
+        sys.exit(-1)
+    
+    if args.term_type not in ["android", "pc"]:
+        logger.error("登录设备类型必须为 android 或 pc")
+        sys.exit(-1)
+    
+    return args.account, args.password, args.term_type, args.log_level, args.interval
 
 
 def main():
     global logger
-    signal.signal(signal.SIGTERM, signal_handler)
-    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, lambda signum, frame: sys.exit(0))
+    signal.signal(signal.SIGINT, lambda signum, frame: sys.exit(0))
 
-    account, password, term_type, log_level = parse_args()
+    account, password, term_type, log_level, interval = parse_args()
 
     set_logger(log_level)
 
-    if not account or not password:
-        logger.error("未指定校园网账户或密码")
-        sys.exit(-1)
-
-    if term_type not in ["android", "pc"]:
-        logger.error("登录设备类型必须为 android 或 pc")
-        sys.exit(-1)
-
-    # 每5秒检查一次网络状态, 如果掉线则重新认证
-    interval = 5
-
-    first = True
-    try:
-        while True:
-            if not is_internet_connected():
-                ip = get_ip()
-                if not ip:
-                    logger.warning("获取IP失败, 请检查 DHCP 是否正常")
-                    continue
-                logger.info(f"正在认证: 账户({account}), 设备类型({term_type}), 设备IP({ip})")
-                success, response = login(account, password, term_type, ip)
-                if not success:
-                    logger.warning(f"认证失败: {response}, {interval}秒后重试...")
-                else:
-                    logger.info(f"认证成功: {get_account()}")
-            else:
-                logger.debug(f"该网络已认证, 认证账户: {get_account()}")
-            if first:
-                first = False
-                logger.info(f"每{interval}秒检查一次网络状态, 如果掉线则重新认证")
-                logger.info(f"如果需要停止程序, 请使用 Ctrl+C 停止")
+    logger.info(f"每{interval}秒检查一次网络状态, 如果掉线则重新认证, CTRL+C 停止程序")
+    while True:
+        # 如果网络已经认证, 则不再重复认证
+        if is_internet_connected():
+            logger.debug(f"该网络已认证, 认证账户: {get_account()}, {interval}秒后重新检查网络状态...")
             time.sleep(interval)
-    except KeyboardInterrupt:
-        logger.info("KeyboardInterrupt received (Ctrl+C), exiting...")
-        sys.exit(0)
+            continue
+
+        # 获取待认证的校园网 IP, 如果获取失败则等待 interval 秒后重试
+        ip = get_ip()
+        if not ip:
+            logger.warning(f"认证失败: 无法获取校园网 IP, 请检查 DHCP 是否正常, {interval}秒后重试...")
+            time.sleep(interval)
+            continue
+
+        # 认证校园网
+        logger.info(f"正在认证: 账户({account}), 设备类型({term_type}), 校园网IP({ip})")
+        result, msg = login(account, password, term_type, ip)
+        
+        # 如果失败原因为账号不存在或密码错误, 则直接退出程序
+        if not result and msg in ["账号不存在", "密码错误"]:
+            logger.warning(f"认证失败: {msg}")
+            sys.exit(-1)
+        elif not result:
+            logger.warning(f"认证失败: {msg}, {interval}秒后重试...")
+        else:
+            logger.info(f"认证成功: {get_account()}, {interval}秒后重新检查网络状态...")
+        time.sleep(interval)
 
 
 if __name__ == "__main__":
